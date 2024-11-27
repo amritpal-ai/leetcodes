dd62cc8318a5e36dac2b4a9a36a925f60c198f6e
amritpal-ai <amritpalbanga17@gmail.com>
2024-11-27 12:52:39 +0530

    Maximum Subarray

---
+++ 53.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentsum=0;
+        int maxsum=nums[0];
+        for (int i=0;i< nums.size();i++){
+            currentsum +=nums[i];
+            maxsum=max(maxsum,currentsum);
+            if(currentsum<0){
+                currentsum=0;
+            }
+        }
+        return maxsum;
+    }
+};
